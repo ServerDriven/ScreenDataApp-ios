091f11dcf28804a7a15b8a8bdbdf09f6e7bf9d34
Zach Eriksen <zmeriksen@icloud.com>
2022-03-30 19:49:02 -0600

    Update SDUI package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(
             url: "https://github.com/ServerDriven/ScreenDataUI-ios",
-            branch: "watchos/support"
+            from: "1.0.0"
         )
     ],
     targets: [
